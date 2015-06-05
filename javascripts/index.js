afbfc8ac91f7a1402d9230d678bab26b4c6cba78
Libby Schumacher-Knight <apprentice@StMac2.local>
2015-06-05 15:23:15 +1200

    game logic

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,25 +2,36 @@
 $(document).ready(function () {
 
   var cards_clicked = [];
+  var img_info_array = [];
   var count = 0;
 
-    placeCard(cardsArray);
-    $('.col-md-3').on('click', function(){
-      var $img_info = $(this).children();
-      $img_info.show();
-      card_click = $(this).attr('class');
-      cards_clicked.push(card_click);
-      if cards_clicked.length(2)
+  placeCard(cardsArray);
+
+  $('.col-md-3').on('click', function(){
+    var $img_info = $(this).children();
+    img_info_array.push($img_info);
+    $img_info.show();
+    card_click = $(this).attr('class');
+    cards_clicked.push(card_click);
+
+    if ($(cards_clicked).length == 2){
+      if ( cards_clicked[0] === cards_clicked[1] ) {
+        $('.cards_clicked[0]').find('#img').css("display", "inline");
+
+      } else {
+
+        $img_info.slideToggle("slow");
+        img_info_array[0].hide();
+        img_info_array = [];
+        console.log(img_info_array);
+        }
+        cards_clicked = [];
+        console.log(cards_clicked);
+    }
 
 
-      console.log(cards_clicked);
-      });
-      //on first click grab class info and store
-
-      //on second click grab class infor and store somewhere else
-      // compare the two variables to see if they are the same
-      //if the same images stay shown
-      //if different images turn down and loop repeats
     });
+
+});
 
 
